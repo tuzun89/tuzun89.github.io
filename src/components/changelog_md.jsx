a3b17902afa0ab5af9e96816906d53c784d2573a
tuzun89 <to.uzunoglu@gmail.com>
2020-08-16 16:36:11 +0100

    added react component 'summary'

--- src/components/changelog_md.jsx
+++ src/components/changelog_md.jsx
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Changelog extends Component {
-    render() {
-        return (
-            <div className="changelog">
-                <h2 className="title flash">CHANGELOG.md</h2>
-                Some significant events
-                <div className="exp">
-                    <div className="loading"> Coming Soon...</div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="changelog">
+        <h2 className="title flash">CHANGELOG.md</h2>
+        Some significant events
+        <div className="exp">
+          <div className="loading"> Coming Soon...</div>
+        </div>
+      </div>
+    );
+  }
 }
-
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Summary extends Component {
-    render() {
-        return (
-            
-        )
-    }
+  render() {
+    return (
+      <div class="container">
+        <h1>Shaunak Pagnis</h1>
+        <h2>
+          Graduate Student at University of Maryland, Baltimore-County, MD.
+        </h2>
+        <div class="intro">
+          <a href="https://internships.github.com/interns/2019/shaunak">
+            Intern
+          </a>{" "}
+          at <a href="https://github.com">GitHub Inc.</a> and previously a Team
+          Lead - Product at <a href="https://amuratech.com">Amura</a>.
+        </div>
+        <div class="links">
+          <a href="https://github.com/shaunakpp">GitHub</a>
+          <a href="https://twitter.com/_kanuahs">Twitter</a>
+          <a href="https://www.linkedin.com/in/shaunak-pagnis/">LinkedIn</a>
+          <a href="mailto:shaunak.pagnis@gmail.com">Email</a>
+        </div>
+      </div>
+    );
+  }
 }
