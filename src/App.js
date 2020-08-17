bb43bcc071672e3b16f2d0cd346a08f828bc5c1d
tuzun89 <to.uzunoglu@gmail.com>
2020-08-17 13:05:09 +0100

    content and dark theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*import logo from './logo.svg';*/
+/*import './App.css';*/
 import Summary from './components/summary'
 import Readme from './components/readme_md'
 import Code from './components/code'
