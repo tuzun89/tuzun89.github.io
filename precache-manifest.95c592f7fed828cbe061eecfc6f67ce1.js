016d6da05686cc28e418a3d75a647c0ddb6ada4f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-22 16:58:19 +0000

    Deploy tuzun89/tuzun89.github.io to github.com/tuzun89/tuzun89.github.io.git:gh-pages

---
+++ precache-manifest.95c592f7fed828cbe061eecfc6f67ce1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962769b9250266273ea574e8b9e41e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f6befd38688f6034c00",
+    "url": "/static/js/2.9f2fdb38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f2fdb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2453de2728fc6fdead7",
+    "url": "/static/js/main.d1599985.chunk.js"
+  },
+  {
+    "revision": "4ac454df26868daa8a26",
+    "url": "/static/js/runtime-main.163cf187.js"
+  }
+]);
