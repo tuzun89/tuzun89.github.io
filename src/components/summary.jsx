ab757f81c441e7aee911d1b43c6b904bbecefffb
tuzun89 <to.uzunoglu@gmail.com>
2021-11-20 17:05:50 +0000

    summary wording updated

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -12,9 +12,9 @@ export default class Summary extends Component {
           MEng Civil Engineering from Heriot-Watt University in Edinburgh, UK.
         </h2>
         <div className="intro">
-          Currently a Structural Engineer at <a href="https://www.elliottwood.co.uk/" rel="noopener noreferrer" target="_blank">ElliottWood </a> 
-          Previously employed at the same capacity at <a href="http://clancy.co.uk/" rel="noopener noreferrer" target="_blank">CCL </a> 
-          and also at <a href="https://www.aksward.com/" rel="noopener noreferrer" target="_blank">AKSW</a>.
+          Currently a Structural Engineer at <a href="https://www.elliottwood.co.uk/" rel="noopener noreferrer" target="_blank">ElliottWood</a>.
+          <p>Previously employed at the same capacity at <a href="http://clancy.co.uk/" rel="noopener noreferrer" target="_blank">CCL </a>
+          and also at <a href="https://www.aksward.com/" rel="noopener noreferrer" target="_blank">AKSW</a>.</p>
           <p>An aspiring  Software Developer - looking for opportunities at a Junior level.</p>
         </div>
         <div className="links">
